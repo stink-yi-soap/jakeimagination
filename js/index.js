7081c185023322600609999bc5d832092df382eb
Yichan Wang <wangy220@newschoo.edu>
2019-08-16 09:47:48 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,63 @@
+$(document).ready (function () {
+
+    $("#nose").click(function(){
+        $("#instruction").html("SQUEEZE ME");
+    })
+
+    $("#redButton").mouseover(function(){
+        alert("DO NOT ABUSE JAKE'S IMAGINATION CONTROL");
+    })
+
+    var loopSwitch = 0; 
+    let sTorso = $(".stretchTorso");
+    function loop() {
+        if (loopSwitch == 1 ) {
+            sTorso.animate({height:'500vw', opacity:'0.4'}, "fast");
+            sTorso.animate({width:'40vw', opacity:'0.9'}, "fast");
+            sTorso.animate({width:'700vw', opacity:'0.3'}, "fast");
+            sTorso.animate({height:'20vw', opacity:'0.9'}, "fast");
+            sTorso.animate({width:'70vw', height:'30vw', opacity:'1'}, "fast", function() { loop() });
+        } else {
+            sTorso.mouseover(function(){
+                sTorso.animate({width:'300vw', height:'340vw'}, "slow");})
+            }
+    }
+    
+
+    $("#on").click(function(){
+        if ($(window).width() <= 1400 && $(window).width() >= 1024){
+            $("body").css('background-image', 'url(/assets/knifeRain.gif)');
+            $("#panel").html("KEEP SQUEEZING");
+        } else {
+            if ($(window).width() < 1024 && $(window).width() > 750){
+            loopSwitch = 1;
+            loop();
+            }
+        }
+    })
+
+    $("#off").click(function(){
+        if ($(window).width() <= 1400 && $(window).width() >= 1024){
+            $("body").css('background-image', 'none');
+            $("#panel").html(" ");
+        } else {
+            if ($(window).width() < 1024 && $(window).width() > 750){
+                loopSwitch = 0;
+                loop();
+            } 
+        }
+    })
+
+
+
+    $("#nonono").click(function(){
+        $("#warn").html("PULL ME OUT!");
+    })
+
+
+
+
+
+
+
+})
